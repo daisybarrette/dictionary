7005850fc8401795fa4d01b70c8c7491431296e2
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-08-22 15:47:24 -0400

    Delete console statements

--- src/functions/fetchDefinition.tsx
+++ src/functions/fetchDefinition.tsx
@@ -5,16 +5,11 @@ const emptyDefinition = {
 }
 
 export default async function fetchDefinition(word: string) {
-    console.log('IN FETCH FUNCTION ', word)
-
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
 
     const result = await response.json()
 
-    console.log('result from api', result)
-
     if (result?.title === 'No Definitions Found') {
-        // @TODO should display error message for user
         return emptyDefinition
     }
 
