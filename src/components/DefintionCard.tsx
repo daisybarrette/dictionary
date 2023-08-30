e32a2da187a28c26cd003e9894f3807a81c7f334
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-08-30 12:58:09 -0400

    First pass at adding audio player

--- src/components/DefintionCard.tsx
+++ src/components/DefintionCard.tsx
@@ -2,12 +2,28 @@ type DefinitionCardProps = {
     definition: {
         word: string,
         phonetic: string,
+        // audio?: string, //// should pass like this from fetch func
+
+        // @TODO specify structure
+        phonetics: Array<object>,
 
         // @TODO specify structure
         meanings: Array<object>,
     },
 }
 
+
+// @TODO extract to separate component
+function AudioPlayer({ audioSrc }: { audioSrc: string }) {
+    console.log('audio src', audioSrc)
+    return (
+        <figure>
+            <figcaption>Play audio:</figcaption>
+            <audio controls src={audioSrc}>
+            </audio>
+        </figure>
+    )
+}
 
 function formatDefinition({ definition }: DefinitionCardProps) {
     // @TODO clean up types
@@ -17,11 +33,31 @@ function formatDefinition({ definition }: DefinitionCardProps) {
     const partOfSpeech = firstMeaning['partOfSpeech']
     const def1 = firstMeaning['definitions'][0]['definition']
 
+    // const hasAudio = !!definition.audio
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+    const phonetics = definition.phonetics[0]
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+    console.log(phonetics)
+
+    //// phonetics may be undefined
+
+    const audioSrc = phonetics ? phonetics['audio'] : null
+    const hasAudio = !!audioSrc //check if valid
+
+
+
     // @TODO extract to presentational component
     return <div className="definition">
         <div className="word">{definition.word}</div>
 
         <div>{definition.phonetic}</div>
+
+        {hasAudio ? <AudioPlayer audioSrc={audioSrc} /> : <> </>}
+
+
 
         <div>{partOfSpeech}</div>
 
