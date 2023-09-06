8c9b1b1bcbccddad269774750aeaf08c5519a06b
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-09-06 11:31:21 -0400

    Add expand/collapse functionality

--- src/components/AccordionContainer.tsx
+++ src/components/AccordionContainer.tsx
@@ -7,24 +7,26 @@ type AccordionItemProps = {
 }
 
 function AccordionItem({ title, content }: AccordionItemProps) {
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const classNameForContent = isExpanded ? 'accordion-item-content is-expanded' : 'accordion-item-content'
+
     return (
         <li className="accordion-item">
-            <div className="accordion-item-title">{title}</div>
-            <div className="accordion-item-content">{content}</div>
+            <button className="accordion-item-title" onClick={() => setIsExpanded(!isExpanded)}>{title}</button>
+
+            <div className={classNameForContent}>
+                {isExpanded && content}
+            </div>
         </li>
     )
 }
 
-
 export default function AccordionContainer() {
-
     return (
         <ul className="accordion-container">
             <AccordionItem title="my cool item title" content="this is the content" />
             <AccordionItem title="another item title" content="and here is the content for that second item" />
-
-            {/* <li className="accordion-item"><div className="accordion-item-title">my cool item title</div> <div className="accordion-item-content">this is the content</div></li> */}
-            {/* <li className="accordion-item"><div className="accordion-item-title">another item title</div> <div className="accordion-item-content">and here is the content for that second item</div></li> */}
         </ul>
     )
 }
