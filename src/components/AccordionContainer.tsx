265a6056378470ff3c8a1142d6d8f04270df1fc4
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-09-06 12:42:21 -0400

    Add some styles, switch to show more/less instead of title

--- src/components/AccordionContainer.tsx
+++ src/components/AccordionContainer.tsx
@@ -10,10 +10,11 @@ function AccordionItem({ title, content }: AccordionItemProps) {
     const [isExpanded, setIsExpanded] = useState(false)
 
     const classNameForContent = isExpanded ? 'accordion-item-content is-expanded' : 'accordion-item-content'
+    const conditionalTitle = isExpanded ? 'Show less' : 'Show more'
 
     return (
         <li className="accordion-item">
-            <button className="accordion-item-title" onClick={() => setIsExpanded(!isExpanded)}>{title}</button>
+            <button className="accordion-item-title" onClick={() => setIsExpanded(!isExpanded)}>{conditionalTitle}</button>
 
             <div className={classNameForContent}>
                 {isExpanded && content}
@@ -25,8 +26,8 @@ function AccordionItem({ title, content }: AccordionItemProps) {
 export default function AccordionContainer() {
     return (
         <ul className="accordion-container">
-            <AccordionItem title="my cool item title" content="this is the content" />
-            <AccordionItem title="another item title" content="and here is the content for that second item" />
+            <AccordionItem title="See more meanings" content="this is the content" />
+            {/* <AccordionItem title="another item title" content="and here is the content for that second item" /> */}
         </ul>
     )
 }
